4d70f5ddf9bf9ebda0c6f7d34232e4b5a7ecaf0b
rafalbielech <rbielech@gmail.com>
2020-12-03 21:04:01 -0500

    fix error

--- app/views.py
+++ app/views.py
@@ -20,11 +20,12 @@ def index(request):
     alias_to_ip_map --> used to populate alias_to_ip_map
     access_tokens --> used to populate tokens
     """
-    rtsp_cameras_on_network = [
-        item.get("id")
-        for item in settings.CONFIG.get("local", {}).get("rtsp_camera", [])
-        if getmacbyip(item.get("ip")) != None
-    ]
+    rtsp_cameras_on_network = []
+    # rtsp_cameras_on_network = [
+    #     item.get("id")
+    #     for item in settings.CONFIG.get("local", {}).get("rtsp_camera", [])
+    #     if getmacbyip(item.get("ip")) != None
+    # ]
 
     return render(
         request,
